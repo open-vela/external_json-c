8a1caea968351c31ba5ae7729644216c46ce8973
openvela-robot <openvela-robot@xiaomi.com>
2019-01-23 22:47:27 +0800

    Merge pull request #473 from besser82/fixup/docs_utf8
    Convert ChangeLog to valid UTF-8 encoding.

--- tests/test_deep_copy.c
+++ tests/test_deep_copy.c
@@ -18,7 +18,6 @@ static const char *json_str1 =
 "        \"GlossDiv\": {"
 "            \"title\": \"S\","
 "            \"null_obj\": null, "
-"            \"null_str\": \" \\u0000 \","
 "            \"GlossList\": {"
 "                \"GlossEntry\": {"
 "                    \"ID\": \"SGML\","
