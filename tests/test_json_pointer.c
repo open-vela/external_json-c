7b531bb8a113f4b0dfffc4b813fa9f239ece1483
openvela-robot <openvela-robot@xiaomi.com>
2020-03-01 08:51:26 +0800

    Merge pull request #542 from dota17/adduint64_final
    add uint64 data to json-c

--- tests/test_json_pointer.c
+++ tests/test_json_pointer.c
@@ -1,11 +1,6 @@
 #include "strerror_override.h"
 #include "strerror_override_private.h"
-#ifdef NDEBUG
-#undef NDEBUG
 #include <assert.h>
-#else
-#include <assert.h>
-#endif
 #include <stdio.h>
 #include <string.h>
 
