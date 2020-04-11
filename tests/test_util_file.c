99415454c5f80f19d6a9da47b78b46942c4558c8
liujinye <liujinye@xiaomi.com>
2020-04-11 14:18:37 +0800

    Merge pull request #575 from besser82/topic/besser82/cosmetics
    Small cosmetics.

--- tests/test_util_file.c
+++ tests/test_util_file.c
@@ -138,12 +138,6 @@ int main(int argc, char **argv)
 	}
 	testdir = argv[1];
 
-	//	Test json_c_version.c
-	if (strncmp(json_c_version(), JSON_C_VERSION, sizeof(JSON_C_VERSION)))
-		return EXIT_FAILURE;
-	if (json_c_version_num() != JSON_C_VERSION_NUM)
-		return EXIT_FAILURE;
-
 	test_read_valid_with_fd(testdir);
 	test_read_valid_nested_with_fd(testdir);
 	test_read_nonexistant();
