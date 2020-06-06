bfaa8d2e86de17ad2c262d968b1e83e4e83710dc
liujinye <liujinye@xiaomi.com>
2020-06-07 02:42:58 +0800

    Kick json_type_string out of struct json_object. The default is now that string data is stored inline at the end of json_object, though to allow for json_object_set_string() to set a _longer_ string, we still need to allow for the possibility of a separate char * pointer. All json types have been split out now, next step it cleanup.

--- json_object_private.h
+++ json_object_private.h
@@ -36,41 +36,74 @@ typedef enum json_object_int_type
 	json_object_int_type_uint64
 } json_object_int_type;
 
-struct json_object
+struct json_object_base // XAX rename to json_object after conversion
 {
+	int newold;  // XAX temporary, remove after code conversion
 	enum json_type o_type;
 	uint32_t _ref_count;
 	json_object_private_delete_fn *_delete;
 	json_object_to_json_string_fn *_to_json_string;
 	struct printbuf *_pb;
-	union data
+	json_object_delete_fn *_user_delete;
+	void *_userdata;
+	char data[1]; // Actually the rest of a struct json_object_${o_type}
+};
+
+struct json_object_object
+{
+	struct json_object_base base;
+	struct lh_table *c_object;
+};
+struct json_object_array
+{
+	struct json_object_base base;
+	struct array_list *c_array;
+};
+
+struct json_object_boolean
+{
+	struct json_object_base base;
+	json_bool c_boolean;
+};
+struct json_object_double
+{
+	struct json_object_base base;
+	double c_double;
+};
+struct json_object_int
+{
+	struct json_object_base base;
+	enum json_object_int_type cint_type;
+	union
 	{
-		json_bool c_boolean;
-		double c_double;
-		struct
-		{
-			union
-			{
-				int64_t c_int64;
-				uint64_t c_uint64;
-			} cint;
-			enum json_object_int_type cint_type;
-		} c_int;
-		struct lh_table *c_object;
-		struct array_list *c_array;
-		struct
-		{
-			union
-			{
-				/* optimize: if we have small strings, we can store them
-				 * directly. This saves considerable CPU cycles AND memory.
-				 */
-				char *ptr;
-				char data[LEN_DIRECT_STRING_DATA];
-			} str;
-			int len;
-		} c_string;
-	} o;
+		int64_t c_int64;
+		uint64_t c_uint64;
+	} cint;
+};
+struct json_object_string
+{
+	struct json_object_base base;
+	ssize_t len;  // Signed b/c negative lengths indicate data is a pointer
+	// Consider adding an "alloc" field here, if json_object_set_string calls
+	// to expand the length of a string are common operations to perform.
+	union
+	{
+		char idata[1]; // Immediate data.  Actually longer
+		char *pdata;   // Only when len < 0
+	} c_string;
+};
+
+struct json_object
+{
+	int newold;
+	enum json_type o_type;
+	uint32_t _ref_count;
+	json_object_private_delete_fn *_delete;
+	json_object_to_json_string_fn *_to_json_string;
+	struct printbuf *_pb;
+		int dummyval; // XAX temp spacer to catch casting errors
+		int du1mmyval; // XAX spacer
+		int d2ummyval; // XAX spacer
 	json_object_delete_fn *_user_delete;
 	void *_userdata;
 };
