4439d40bde83225fdcb81bdef9bbfca2f52a9213
openvela-robot <openvela-robot@xiaomi.com>
2020-06-14 17:56:35 +0800

    Merge pull request #634 from micahsnyder/json-c-fPIC
    Issue #508: `-fPIC` to link libjson-c.a with libs

--- json_tokener.c
+++ json_tokener.c
@@ -630,6 +630,8 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 
 		case json_tokener_state_escape_unicode:
 		{
+			unsigned int got_hi_surrogate = 0;
+
 			/* Handle a 4-byte sequence, or two sequences if a surrogate pair */
 			while (1)
 			{
@@ -641,24 +643,14 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 					{
 						unsigned char unescaped_utf[4];
 
-						if (tok->got_hi_surrogate)
+						if (got_hi_surrogate)
 						{
 							if (IS_LOW_SURROGATE(tok->ucs_char))
 							{
-								/* remove the utf8_replacement_char */
-								/* which may generate during */
-								/* parsing the high surrogate pair. */
-								if (!strcmp(
-								        tok->pb->buf,
-								        (char *)
-								            utf8_replacement_char))
-								{
-									printbuf_reset(tok->pb);
-								}
 								/* Recalculate the ucs_char, then fall thru to process normally */
 								tok->ucs_char =
 								    DECODE_SURROGATE_PAIR(
-								        tok->got_hi_surrogate,
+								        got_hi_surrogate,
 								        tok->ucs_char);
 							}
 							else
@@ -670,7 +662,7 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 								    (char *)utf8_replacement_char,
 								    3);
 							}
-							tok->got_hi_surrogate = 0;
+							got_hi_surrogate = 0;
 						}
 
 						if (tok->ucs_char < 0x80)
@@ -694,7 +686,7 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 							 * the beginning of another sequence, which
 							 * should be the low surrogate.
 							 */
-							tok->got_hi_surrogate = tok->ucs_char;
+							got_hi_surrogate = tok->ucs_char;
 							/* Not at end, and the next two chars should be "\u" */
 							if ((len == -1 ||
 							     len > (tok->char_offset + 2)) &&
@@ -725,8 +717,6 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 									    (char *)
 									        utf8_replacement_char,
 									    3);
-									tok->ucs_char = 0;
-									tok->st_pos = 0;
 									goto out;
 								}
 								tok->ucs_char = 0;
@@ -796,8 +786,7 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 				if (!ADVANCE_CHAR(str, tok) || !PEEK_CHAR(c, tok))
 				{
 					/* Clean up any pending chars */
-					if (tok->got_hi_surrogate &&
-					    strcmp(tok->pb->buf, (char *)utf8_replacement_char))
+					if (got_hi_surrogate)
 						printbuf_memappend_fast(
 						    tok->pb, (char *)utf8_replacement_char, 3);
 					goto out;
