ce629d65e3edf3e500885ff44b1dbb8c748f9f36
openvela-robot <openvela-robot@xiaomi.com>
2021-07-25 19:07:06 +0800

    Merge some old work to include (some of) PR #464 into the current master branch.

--- json_tokener.c
+++ json_tokener.c
@@ -17,7 +17,6 @@
 
 #include "math_compat.h"
 #include <assert.h>
-#include <ctype.h>
 #include <limits.h>
 #include <math.h>
 #include <stddef.h>
@@ -53,6 +52,27 @@
 #error You do not have strncasecmp on your system.
 #endif /* HAVE_STRNCASECMP */
 
+/* The following helper functions are used to speed up parsing. They
+ * are faster than their ctype counterparts because they assume that
+ * the input is in ASCII and that the locale is set to "C". The
+ * compiler will also inline these functions, providing an additional
+ * speedup by saving on function calls.
+ */
+static inline int is_ws_char(char c)
+{
+	return c == ' '
+	    || c == '\t'
+	    || c == '\n'
+	    || c == '\r';
+}
+
+static inline int is_hex_char(char c)
+{
+	return (c >= '0' && c <= '9')
+	    || (c >= 'A' && c <= 'F')
+	    || (c >= 'a' && c <= 'f');
+}
+
 /* Use C99 NAN by default; if not available, nan("") should work too. */
 #ifndef NAN
 #define NAN nan("")
@@ -61,7 +81,8 @@
 static const char json_null_str[] = "null";
 static const int json_null_str_len = sizeof(json_null_str) - 1;
 static const char json_inf_str[] = "Infinity";
-static const char json_inf_str_lower[] = "infinity";
+/* Swapped case "Infinity" to avoid need to call tolower() on input chars: */
+static const char json_inf_str_invert[] = "iNFINITY";
 static const unsigned int json_inf_str_len = sizeof(json_inf_str) - 1;
 static const char json_nan_str[] = "NaN";
 static const int json_nan_str_len = sizeof(json_nan_str) - 1;
@@ -322,7 +343,7 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 
 		case json_tokener_state_eatws:
 			/* Advance until we change state */
-			while (isspace((unsigned char)c))
+			while (is_ws_char(c))
 			{
 				if ((!ADVANCE_CHAR(str, tok)) || (!PEEK_CHAR(c, tok)))
 					goto out;
@@ -427,17 +448,15 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 			 * complicated with likely little performance benefit.
 			 */
 			int is_negative = 0;
-			const char *_json_inf_str = json_inf_str;
-			if (!(tok->flags & JSON_TOKENER_STRICT))
-				_json_inf_str = json_inf_str_lower;
 
 			/* Note: tok->st_pos must be 0 when state is set to json_tokener_state_inf */
 			while (tok->st_pos < (int)json_inf_str_len)
 			{
 				char inf_char = *str;
-				if (!(tok->flags & JSON_TOKENER_STRICT))
-					inf_char = tolower((unsigned char)*str);
-				if (inf_char != _json_inf_str[tok->st_pos])
+				if (inf_char != json_inf_str[tok->st_pos] &&
+				    ((tok->flags & JSON_TOKENER_STRICT) ||
+				      inf_char != json_inf_str_invert[tok->st_pos])
+				   )
 				{
 					tok->err = json_tokener_error_parse_unexpected;
 					goto out;
@@ -653,7 +672,7 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 			/* Handle a 4-byte \uNNNN sequence, or two sequences if a surrogate pair */
 			while (1)
 			{
-				if (!c || !strchr(json_hex_chars, c))
+				if (!c || !is_hex_char(c))
 				{
 					tok->err = json_tokener_error_parse_string;
 					goto out;
@@ -926,7 +945,7 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 				next call to json_tokener_parse().
 			 */
 			if (tok->depth > 0 && c != ',' && c != ']' && c != '}' && c != '/' &&
-			    c != 'I' && c != 'i' && !isspace((unsigned char)c))
+			    c != 'I' && c != 'i' && !is_ws_char(c))
 			{
 				tok->err = json_tokener_error_parse_number;
 				goto out;
