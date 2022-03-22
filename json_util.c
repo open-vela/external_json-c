8017cfa3d24ce16686f81308930a413bee14ce60
liujinye <liujinye@xiaomi.com>
2022-03-23 00:14:36 +0800

    Merge pull request #760 from BonsaiYi/error
    Code style: removed unneeded double-quotes

--- json_util.c
+++ json_util.c
@@ -101,22 +101,15 @@ struct json_object *json_object_from_fd_ex(int fd, int in_depth)
 	if (!tok)
 	{
 		_json_c_set_last_err(
-		    "json_object_from_fd_ex: unable to allocate json_tokener(depth=%d): %s\n",
-		    depth, strerror(errno));
+		    "json_object_from_fd_ex: unable to allocate json_tokener(depth=%d): %s\n", depth,
+		    strerror(errno));
 		printbuf_free(pb);
 		return NULL;
 	}
 
 	while ((ret = read(fd, buf, JSON_FILE_BUF_SIZE)) > 0)
 	{
-		if (printbuf_memappend(pb, buf, ret) < 0)
-		{
-			_json_c_set_last_err("json_object_from_fd_ex: error reading fd %d: %s\n",
-			                     fd, strerror(errno));
-			json_tokener_free(tok);
-			printbuf_free(pb);
-			return NULL;
-		}
+		printbuf_memappend(pb, buf, ret);
 	}
 	if (ret < 0)
 	{
